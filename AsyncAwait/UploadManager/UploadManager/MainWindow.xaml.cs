89cefda6e20bd00468bafa94d48211ac254a9791
Uladzimir_Zaitsau <uladzimir_zaitsau1@epam.com>
2019-04-11 11:45:55 +0300

    refactoring for uploadmanager

--- AsyncAwait/UploadManager/UploadManager/MainWindow.xaml.cs
+++ AsyncAwait/UploadManager/UploadManager/MainWindow.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using Microsoft.Win32;
 
 namespace UploadManager
 {
@@ -13,74 +10,56 @@ namespace UploadManager
     // которую необходимо загрузить. В процессе загрузки пользователь может ее отменить. 
     // Пользователь может задавать несколько источников для закачки. 
     // Скачивание страниц не должно блокировать интерфейс приложения.
+
+    // Save files to \bin\debug folder with random name as *.html
     public partial class MainWindow : Window
     {
-        private delegate void CancelHandler();
-        private event CancelHandler Cancel;
+        private readonly WebPageUploader _webPageUploader;
 
         public MainWindow()
         {
             InitializeComponent();
+            _webPageUploader = new WebPageUploader();
+            _webPageUploader.OnCancelUpload += OnCancelUploadHandler;
+            _webPageUploader.OnSuccessfullUpload += OnSuccessfullUploadHandler;
         }
 
         private async void Button_Upload_Click(object sender, RoutedEventArgs e)
         {
-            //var saveFileDialog = new SaveFileDialog { DefaultExt = ".html" };
-
-            //if (saveFileDialog.ShowDialog() == true)
-            //{
-                try
-                {
-                    var url1 = new Uri(UrlTxtBox1.Text);
-                    var url2 = new Uri(UrlTxtBox2.Text);
-                    var url3 = new Uri(UrlTxtBox3.Text);
-                    var content1 = await DownloadDataAsync(url1);
-                    var content2 = await DownloadDataAsync(url2);
-                    var content3 = await DownloadDataAsync(url3);
-
-                    var task1 = Task.Run(() => File.WriteAllText($"{Guid.NewGuid()}.html", content1));
-                    var task2 = Task.Run(() => File.WriteAllText($"{Guid.NewGuid()}.html", content2));
-                    var task3 = Task.Run(() => File.WriteAllText($"{Guid.NewGuid()}.html", content3));
-
-                    await Task.WhenAll(task1, task2, task3);
-
-                    Logs.Items.Add($"Web pages: {url1}|{url2}|{url3} were downloaded successfully.");
-                }
-                catch (Exception exception)
-                {
-                    Logs.Items.Add($"{exception.Message}");
-                }
-            //}
-        }
-
-        private async Task<string> DownloadDataAsync(Uri uri)
-        {
-            var content = string.Empty;
-
-            if (string.IsNullOrWhiteSpace(uri.ToString()))
-                throw new ArgumentNullException(nameof(uri), @"Uri can not be null or empty.");
-
-            if (!Uri.IsWellFormedUriString(uri.ToString(), UriKind.Absolute))
-                throw new ArgumentException(nameof(uri), @"Uri si not well-formed.");
-
-            using (var webClient = new WebClient())
+            try
             {
-                Cancel += webClient.CancelAsync;
-                var dataArr = await webClient.DownloadDataTaskAsync(uri);
-                content = Encoding.UTF8.GetString(dataArr);
-            }
+                var url1 = new Uri(UrlTxtBox1.Text);
+                var url2 = new Uri(UrlTxtBox2.Text);
+                var url3 = new Uri(UrlTxtBox3.Text);
 
-            return content;
+                await Task.WhenAll(
+                    _webPageUploader.UploadAsync(url1),
+                    _webPageUploader.UploadAsync(url2), 
+                    _webPageUploader.UploadAsync(url3));
+            }
+            catch (WebException ex) when (ex.Status == WebExceptionStatus.RequestCanceled)
+            {
+                // ignore this exception
+            }
+            catch (Exception exception)
+            {
+                Logs.Items.Add($"Unexpected exception occurs: {exception.Message}");
+            }
         }
 
         private void Button_Cancel_Click(object sender, RoutedEventArgs e)
         {
-            OnCancelEvent();
+            _webPageUploader.Cancel();
         }
 
-        protected virtual void OnCancelEvent()
+        private void OnCancelUploadHandler(object sender, string url)
         {
-            Cancel?.Invoke();
+            Logs.Items.Add($"Web page upload was canceled: {url}.");
+        }
+
+        private void OnSuccessfullUploadHandler(object sender, string url)
+        {
+            Logs.Items.Add($"Web page: {url} was downloaded successfully.");
         }
     }
 }
---
+++ AsyncAwait/UploadManager/UploadManager/WebPageUploader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadManager
+{
+    public class WebPageUploader
+    {
+        private readonly ConcurrentDictionary<Guid, (string url, Action cancel)> _cancelActions = 
+            new ConcurrentDictionary<Guid, (string url, Action cancel)>();
+
+        public EventHandler<string> OnSuccessfullUpload;
+        public EventHandler<string> OnCancelUpload;
+
+        public async Task UploadAsync(Uri uri)
+        {
+            string content;
+
+            if (string.IsNullOrWhiteSpace(uri.ToString()))
+                throw new ArgumentNullException(nameof(uri), @"Uri can not be null or empty.");
+
+            if (!Uri.IsWellFormedUriString(uri.ToString(), UriKind.Absolute))
+                throw new ArgumentException(nameof(uri), @"Uri si not well-formed.");
+
+            using (var webClient = new WebClient())
+            {
+                var guid = Guid.NewGuid();
+                _cancelActions[guid] = (uri.ToString(), () => webClient.CancelAsync());
+                var dataArr = await webClient.DownloadDataTaskAsync(uri);
+                _cancelActions.TryRemove(guid, out var item);
+                content = Encoding.UTF8.GetString(dataArr);
+                File.WriteAllText($"{Guid.NewGuid()}.html", content);
+            }
+
+            OnSuccessfullUpload?.Invoke(this, uri.ToString());
+        }
+
+        public void Cancel()
+        {
+            foreach (var (url, cancel) in _cancelActions.Values)
+            {
+                cancel();
+                OnCancelUpload(this, url);
+            }
+
+            _cancelActions.Clear();
+        }
+    }
+}
